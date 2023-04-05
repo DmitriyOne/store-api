909f19d0fce0d26a7ec2ebb6cf5fc836669cfb8e
Дмитрий Фенько <senkoner4@gmail.com>
2023-04-05 14:22:36 +0300

    Footer

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,13 +1,25 @@
 import { Container } from '@components'
 
+import { STORE_LINKS } from '@constants/links'
+
 import styles from './footer.module.scss'
 
 export const Footer = () => {
 
 	return (
-		<footer>
-			<Container>
-				Footer
+		<footer className={styles.component}>
+			<Container justify="space-between">
+				<div className={styles.text}>
+					Copyright © 2023
+				</div>
+
+				<a
+					className={styles.text}
+					href={STORE_LINKS.instagram}
+					target="_blank"
+				>
+					Frontend: Dima F.
+				</a>
 			</Container>
 		</footer>
 	)
---
+++ src/constants/links.ts
@@ -0,0 +1,3 @@
+export const STORE_LINKS = {
+	instagram: 'https://www.instagram.com/freelancer_fenko',
+}
