64551ffe11af607ccbc0d00fc66375367c46d306
Дмитрий Фенько <senkoner4@gmail.com>
2023-04-05 12:00:24 +0300

    Alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
 import type { AppProps } from 'next/app'
 
-import { Layout } from '@/components/Layout'
+import { Layout } from '@components'
 
-import '@/styles/global.scss'
+import '@styles/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
 
