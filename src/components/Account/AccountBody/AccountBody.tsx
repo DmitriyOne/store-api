7bc968a8cacc347907b6a73d19070a8cb2bd1142
Дмитрий Фенько <senkoner4@gmail.com>
2023-05-03 23:00:57 +0300

    Styles account page

--- src/components/Account/AccountBody/AccountBody.tsx
+++ src/components/Account/AccountBody/AccountBody.tsx
@@ -1,57 +1,45 @@
 import { FC } from 'react'
-import { FaCheck } from 'react-icons/fa'
 
-import { Box, Flex, Icon, Text } from '@chakra-ui/react'
+import { Box, Text } from '@chakra-ui/react'
 
 import { IUserState } from '@interfaces'
 
-import { emailWrapperStyles, iconStyles, nameStyles, textSmallStyles, textStyles } from './account-body.styles'
+import { useWindowSize } from '@hooks'
+
+import { AccountVerifiedData } from '../AccountVerifiedData'
+
+import { componentStyles, nameStyles, textStyles } from './account-body.styles'
 
 export const AccountBody: FC<IUserState> = ({ ...user }) => {
+	const { isDesktop } = useWindowSize()
 
 	const date1 = new Date(user.createAccount)
 	const date2 = new Date(user.lastLogin)
 
 	return (
-		<Box>
-			<Text {...nameStyles}>
-				{user.name}
-			</Text>
-
-			<Text {...textStyles}>
-				{user.email}
-			</Text>
-			{user.isEmailVerified ? (
-				<Flex {...emailWrapperStyles}>
-					<Icon
-						as={FaCheck}
-						{...iconStyles}
-					/>
-					<Text as="small"
-						{...textSmallStyles}
-					>
-						Email verified
-					</Text>
-				</Flex>
-			) : (
-				<Text
-					as="small"
-					{...textSmallStyles}
-				>
-					Email not verified
+		<Box {...componentStyles}>
+			{isDesktop &&
+				<Text {...nameStyles}>
+					{user.name}
 				</Text>
-			)}
+			}
 
 			<Text {...textStyles}>
-				{user.phone ?? 'Phone number not provided'}
+				<b>Email:</b> {user.email}
+			</Text>
+			<AccountVerifiedData variant="email" />
+
+			<Text {...textStyles}>
+				<b>Phone:</b> {user.phone ?? 'number not provided'}
+			</Text>
+			<AccountVerifiedData variant="phone" />
+
+			<Text {...textStyles}>
+				<b>Account created on</b> {date1.toLocaleString()}
 			</Text>
 
 			<Text {...textStyles}>
-				Account created on {date1.toLocaleString()}
-			</Text>
-
-			<Text {...textStyles}>
-				Last login on {date2.toLocaleString()}
+				<b>Last login on</b> {date2.toLocaleString()}
 			</Text>
 		</Box>
 
