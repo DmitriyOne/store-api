74b8e3739cf6c1d98e486f50985b7fa81b4cb52f
Дмитрий Фенько <senkoner4@gmail.com>
2023-05-04 19:43:38 +0300

    Account item active

--- src/components/Layout/Header/MenuAccount/MenuAccount.tsx
+++ src/components/Layout/Header/MenuAccount/MenuAccount.tsx
@@ -1,17 +1,15 @@
-import NextLink from 'next/link'
-
 import { Avatar, Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 
 import { useAuth } from '@hooks'
 
-import { menuItem } from './menu'
+import { ActiveLink } from '@components'
 
 import { buttonStyles } from './menu-account.styles'
 
 export const MenuAccount = () => {
-	const {  user, logout } = useAuth()
+	const { user, logout } = useAuth()
 
-	const userName = user.name.toLowerCase().replace(/\s+/g, '').trim()
+	const userName = user.name?.toLowerCase().replace(/\s+/g, '').trim()
 
 	return (
 		<Menu>
@@ -27,25 +25,36 @@ export const MenuAccount = () => {
 				/>
 			</MenuButton>
 			<MenuList>
-				{menuItem.map(item =>
-					<MenuItem
-						as={NextLink}
-						key={item.href}
-						href={{
-							pathname: item.href,
-							query: { displayName: userName },
-						}}
-					>
-						{item.title}
+				<ActiveLink
+					href={`/account/${userName}`}
+					activeClassName="dropdown-menu-link_active"
+				>
+					<MenuItem as="span">
+						Account
 					</MenuItem>
-				)}
+				</ActiveLink>
+				<ActiveLink
+					href={`/account/${userName}/orders`}
+					activeClassName="dropdown-menu-link_active"
+				>
+					<MenuItem as="span">
+						My orders
+					</MenuItem>
+				</ActiveLink>
+				<ActiveLink
+					href={`/account/${userName}/settings`}
+					activeClassName="dropdown-menu-link_active"
+				>
+					<MenuItem as="span">
+						Setting
+					</MenuItem>
+				</ActiveLink>
 				<MenuItem
 					as="button"
 					onClick={logout}
 				>
 					Logout
 				</MenuItem>
-
 			</MenuList>
 		</Menu>
 	)
