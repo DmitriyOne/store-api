12978835aa90a0ebf7c8c7576ebdc7c796f98877
Дмитрий Фенько <senkoner4@gmail.com>
2023-05-09 12:24:36 +0300

    Revert commit

---
+++ firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAY2XHHVix_89j0Nl-TX30-1KY1ulReJEE',
+	authDomain: 'store-api-4baea.firebaseapp.com',
+	projectId: 'store-api-4baea',
+	storageBucket: 'store-api-4baea.appspot.com',
+	messagingSenderId: '209633066913',
+	appId: '1:209633066913:web:c15cbc19b2b5b7aa5395e0',
+}
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const storage = getStorage(app)
