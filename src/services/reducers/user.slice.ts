71fa5e2582921e3c8178b1e184e23fce1d51f6ec
Дмитрий Фенько <senkoner4@gmail.com>
2023-05-12 10:21:34 +0300

    Update user reducer

--- src/services/reducers/user.slice.ts
+++ src/services/reducers/user.slice.ts
@@ -20,31 +20,13 @@ export const userSlice = createSlice({
 	initialState,
 	reducers: {
 		addUser: (state, action: PayloadAction<IUserState>) => {
-			state.id = action.payload.id
-			state.name = action.payload.name
-			state.email = action.payload.email
-			state.isEmailVerified = action.payload.isEmailVerified
-			state.avatar = action.payload.avatar
-			state.phone = action.payload.phone
-			state.isPhoneVerified = action.payload.isPhoneVerified
-			state.createAccount = action.payload.createAccount
-			state.lastLogin = action.payload.lastLogin
-			state.token = action.payload.token
+			Object.assign(state, action.payload)
 		},
 		updateUser: (state, action: PayloadAction<Partial<IUserState>>) => {
 			Object.assign(state, action.payload)
 		},
 		removeUser: (state) => {
-			state.id = null
-			state.name = null
-			state.email = null
-			state.isEmailVerified = null
-			state.avatar = null
-			state.phone = null
-			state.isPhoneVerified = null
-			state.createAccount = null
-			state.lastLogin = null
-			state.token = null
+			Object.assign(state, initialState)
 		},
 	},
 })
